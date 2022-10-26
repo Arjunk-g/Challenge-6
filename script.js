f3e313f9a0e86581cb0c341d6e5374a7d9e2adff
arjun gupta <arjunkarangupta100@gmail.com>
2022-10-25 21:06:29 -0400

    more intro code

---
+++ script.js
@@ -0,0 +1,50 @@
+var header = document.getElementById("heading");
+var displayDayCards = document.getElementById("dayCards");
+var search = document.getElementById("searchBar");
+var apiKey = "aafe4f4c88c16dbb6b700eb070949996";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function bruhHeader(){
+//     Text.style.color = 'white'; 
+// }
+
+// function renderDayCard(){
+//     var bruh = `
+//     <div>
+//     Talk here and insert an item eventuallity
+//     </div>`
+//     displayDayCards.innerhtml += bruh
+// };
+
+// renderDayCard();
+// // function RenderStuff(){
+//     displayDayCard();
+// };
+// RenderStuff();
