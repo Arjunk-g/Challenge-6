0f666089c647624c7cf7dc6af7bfa0fc96f7ea27
arjun gupta <arjunkarangupta100@gmail.com>
2022-10-26 03:55:53 -0400

    got search bar value to display in local storage and now need to retrieve it to inject into queryURL

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 var header = document.getElementById("heading");
 var displayDayCards = document.getElementById("dayCards");
 var search = document.getElementById("searchBar");
-var apiKey = "aafe4f4c88c16dbb6b700eb070949996";
+var APIKey = "aafe4f4c88c16dbb6b700eb070949996";
+// var city = document.getElementById("cityRequested").value;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + parsed + "&appid=" + APIKey;
 
 
 
 
+// fetch(queryURL)
+// .then(data => data.text)
+// .tehn
+// .catch(error => {
+//     // handle the error
+// });
 
+function launch(){
+    var bruh = document.getElementById("cityRequested").value;   
+    // document.getElementById("demo").innerHTML = city;
+    localStorage.setItem("bruh2", JSON.stringify(bruh));
+}
+var outtaHere = localStorage.getItem("bruh2");
+var parsed = JSON.parse(outtaHere);
 
+// var stringed = JSON.stringify(outtaHere);
+// $("#formID").submit(function(e) {
+//     e.preventDefault();
+// });
 
-
-
-
-
-
-
-
-
-
-
+console.log(parsed);
+console.log(queryURL);
 
 
 
